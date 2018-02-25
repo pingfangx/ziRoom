debaec792de65a3ba18d62b7e2b2545c59b637dc
pingfangx <pingfangx@pingfangx.com>
2018-02-25 12:04:20 +0800

    修改扫描方法为先划分，划分到最小再抓房源
    各原来的算法无本质区别，只是更精确地显示进度。

--- ziroom_spider.py
+++ ziroom_spider.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import json
+import math
 import queue
+import sys
 import time
 import zipfile
 
@@ -31,8 +33,9 @@ class Grid:
             return self._page_one_cache
 
         url = API_URL % (lonlat[0], lonlat[1], lonlat[2], lonlat[3], page_index)
-        # print(url)
         while True:
+            sys.stdout.write('\r get %s ' % url)
+            sys.stdout.flush()
             try:
                 json_str = requests.get(url, headers={
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, "
@@ -46,9 +49,11 @@ class Grid:
                     return obj
                 else:
                     print("error %s" % json_str)
+            except requests.exceptions.ReadTimeout:
+                pass
             except Exception as e:
+                print(type(e))
                 print('error:%s' % e)
-                print("retry " + url)
 
     def empty(self):
         obj = self._json_request((self._lon_min, self._lon_max, self._lat_min, self._lat_max), 1)
@@ -107,7 +112,6 @@ class GridManager:
         self._min_area = min_area
         self._split_count = split_count
         self._result = {}
-        self._scanned_area = 0
         self._scan_start_time = 0
 
     def run(self):
@@ -116,34 +120,57 @@ class GridManager:
         这样的结果是，当到达第一个最小区域时，整个队列中都是最小区域块，其中一些可能仍没有房源
         """
         self._scan_start_time = time.time()
-        while not self._q.empty():
-            grid = self._q.get()
-            if not grid.empty():
-                # 判断整个区域
-                if grid.area() > self._min_area:
-                    for item in grid.split(count=self._split_count):
-                        self._q.put(item)
-                else:
-                    temp = grid.get_rooms()
-                    for k in temp.keys():
-                        self._result[k] = temp[k]
-                    self._scanned_area += grid.area()
-                    self._print_progress()
-            else:
-                self._scanned_area += grid.area()
-                self._print_progress()
-        return self._result
 
-    def _print_progress(self):
-        """输出进度"""
-        print("%d / %d = %.2f%% : %d,queue size =%d,spend time %d" % (
-            self._scanned_area, self._total_area, float(self._scanned_area) / self._total_area * 100,
-            len(self._result.keys()), self._q.qsize(), time.time() - self._scan_start_time))
+        # 第一步，划分区块
+
+        # 求需要划分多少轮
+        # 最大面积除以最小面积，得到倍数，再对 划分数量的平方求对数，取 ceil
+        num = int(math.ceil(math.log(self._total_area / self._min_area, self._split_count ** 2)))
+        print('划分区块，共需要划分 %d 轮' % num)
+
+        # 划分至最小块
+        for i in range(num):
+            scan_size = self._q.qsize()
+            print('\n划分第 %d/%d 轮，本轮共需划分 %d 个区块' % (i + 1, num, scan_size))
+
+            smaller_area_queue = queue.Queue()
+            temp_index = 0
+            while not self._q.empty():
+                temp_index += 1
+                grid = self._q.get()
+                if not grid.empty():
+                    # 不为空，划分
+                    print('第 %d/%d 个区块不为空，进行划分' % (temp_index, scan_size))
+                    for item in grid.split(count=self._split_count):
+                        smaller_area_queue.put(item)
+                else:
+                    # 为空
+                    print('第 %d/%d 个区块为空，移除' % (temp_index, scan_size))
+            # 经过上一层循环，q 已为空，新划分的小块全部位于 smaller_area_queue 中
+            self._q = smaller_area_queue
+
+        # 第二步，对划分的所有最小区块抓取房源
+
+        size = self._q.qsize()
+        print('划分区块结束，花费时间 %ds，开始抓取房源，共有 %d 个区块' % (time.time() - self._scan_start_time, size))
+        self._scan_start_time = time.time()
+        scanned_area = 0
+        while not self._q.empty():
+            area = self._q.get()
+            self._result.update(area.get_rooms())
+            print()
+            scanned_area += 1
+            remain_time = (time.time() - self._scan_start_time) / scanned_area * self._q.qsize()
+            print('当前共 %d 个房源，队列剩余 %d 个区块，花费时间 %ds，预计剩余时间 %ds ' % (
+                len(self._result), self._q.qsize(), time.time() - self._scan_start_time, remain_time))
+        return self._result
 
 
 if __name__ == '__main__':
     grid_range = [115.7, 117.4, 39.4, 41.6]  # 北京市范围，扫描别的城市，只要修改经纬度范围即可 参数格式["lon_min,lon_max,lat_min,lat_max"]
 
+    # 测试
+    # gm = GridManager(grid_range, min_area=1e10)
     gm = GridManager(grid_range)
     all_rooms = gm.run()
     rooms = list(filter(lambda x: x["room_status"] != "ycz" and x["room_status"] != "yxd", all_rooms.values()))
@@ -158,3 +185,4 @@ if __name__ == '__main__':
         f.writestr('share_rooms.json', json.dumps(share_rooms))
     with zipfile.ZipFile('web/whole_rooms.zip', 'w', zipfile.ZIP_DEFLATED) as f:
         f.writestr('whole_rooms.json', json.dumps(whole_rooms))
+    print('保存结果完成')
