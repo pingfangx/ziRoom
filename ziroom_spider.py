60823e1b7339bee03a53fb9143151fe456bb503d
pingfangx <pingfangx@pingfangx.com>
2018-02-25 15:10:11 +0800

    添加多线程支持，减少抓取时间

--- ziroom_spider.py
+++ ziroom_spider.py
@@ -2,7 +2,7 @@
 import json
 import math
 import queue
-import sys
+import threading
 import time
 import zipfile
 
@@ -34,9 +34,11 @@ class Grid:
             return self._page_one_cache
 
         url = API_URL % (lonlat[0], lonlat[1], lonlat[2], lonlat[3], page_index)
-        while True:
-            sys.stdout.write('\r get %s ' % url)
-            sys.stdout.flush()
+        retry_time = 0
+        while True and retry_time < 10:
+            retry_time += 1
+            # sys.stdout.write('\r get %s ' % url)
+            # sys.stdout.flush()
             try:
                 json_str = requests.get(url, headers={
                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, "
@@ -68,7 +70,7 @@ class Grid:
     def area(self):
         return (self._lon_max - self._lon_min) * 1e5 * (self._lat_max - self._lat_min) * 1e5
 
-    def get_rooms(self):
+    def get_rooms(self, thread_id):
         """获取房间，会获取各页"""
         result = {}
         page_index = 1
@@ -76,19 +78,37 @@ class Grid:
         useless_count = 0
 
         while True:
-            last_len = len(result.keys())
-
+            last_len = len(result)
             obj = self._json_request((self._lon_min, self._lon_max, self._lat_min, self._lat_max), page_index)
+
+            if obj is None:
+                print('线程 %d 结果为空,跳过 %d 页' % (thread_id, page_index))
+                page_index += 1
+                continue
 
             for item in obj["data"]["rooms"]:
                 result[item["id"]] = item
-            page_index += 1
-            if last_len == len(result.keys()):
+
+            pages = obj["data"]["pages"]
+            if page_index == pages:
+                # 到达最后一页
+                print('线程 %d 获取 %d/%d 页完成,获得 %d 处房源,添加 %d 房源' % (
+                    thread_id, page_index, pages, len(obj['data']['rooms']), len(result) - last_len))
+                return result
+            if len(obj["data"]["rooms"]) == 0:
+                # 没有数据
+                print('线程 %d 获取 %d/%d 页，结果为空' % (thread_id, page_index, pages))
+                return result
+            if last_len == len(result):
                 # 没有产生结果
                 useless_count += 1
-            if len(obj["data"]["rooms"]) == 0 or useless_count > 3:
-                # 结果为空或者超限，则返回
+            if useless_count > 3:
+                # 这里不太清原来的逻辑,可能是 result 更新时 id 相同导致相同数据数量没有添加
+                print('线程 %d 获取 %d/%d 页，无效数据超限' % (thread_id, page_index, pages))
                 return result
+            page_index += 1
+            print('线程 %d 获取 %d/%d 页完成,获得 %d 房源,添加 %d 房源' % (
+                thread_id, page_index, pages, len(obj['data']['rooms']), len(result) - last_len))
 
     def split(self, count=2):
         """分割"""
@@ -108,7 +128,7 @@ class Grid:
 
 
 class GridManager:
-    def __init__(self, lonlat, min_area=1e6, split_count=2):
+    def __init__(self, lonlat, min_area=1e6, split_count=2, thread_num=4):
         self._q = queue.Queue()
         "队列"
 
@@ -117,8 +137,10 @@ class GridManager:
         self._total_area = root_grid.area()
         self._min_area = min_area
         self._split_count = split_count
+        self._thread_num = thread_num
         self._result = {}
         self._scan_start_time = 0
+        self._scanned_area = 0
 
     def run(self):
         """
@@ -164,24 +186,48 @@ class GridManager:
         size = self._q.qsize()
         print('划分区块结束，花费时间 %ds，开始抓取房源，共有 %d 个区块' % (time.time() - self._scan_start_time, size))
         self._scan_start_time = time.time()
-        scanned_area = 0
-        while not self._q.empty():
-            area = self._q.get()
-            self._result.update(area.get_rooms())
-            print()
-            scanned_area += 1
-            remain_time = (time.time() - self._scan_start_time) / scanned_area * self._q.qsize()
-            print('当前共 %d 个房源，队列剩余 %d 个区块，花费时间 %ds，预计剩余时间 %ds ' % (
-                len(self._result), self._q.qsize(), time.time() - self._scan_start_time, remain_time))
+        self._scanned_area = 0
+        threads = []
+        for i in range(0, self._thread_num):
+            worker = threading.Thread(target=self.get_rooms_in_thread, args=(i + 1,))
+            worker.start()
+            threads.append(worker)
+        # 这里好像不需要 thread 的 join，queue 的 join 也会阻塞，但还是加上线程的 join，以使线程结束后再继续执行
+        # https://docs.python.org/3/library/queue.html
+        # http://blog.csdn.net/xiao_huocai/article/details/74781820
+        # block until all tasks are done
+        self._q.join()
+        for t in threads:
+            t.join()
+        print("获取结束")
         return self._result
+
+    def get_rooms_in_thread(self, thread_id):
+        while True:
+            try:
+                self.get_rooms(thread_id)
+            except queue.Empty:
+                print('线程 %d 结束' % thread_id)
+                break
+
+    def get_rooms(self, thread_id):
+        area = self._q.get(block=True, timeout=1)  # 不设置阻塞的话会一直去尝试获取资源
+        self._scanned_area += 1
+        print('线程 %d 获取第 %d 个区块' % (thread_id, self._scanned_area))
+        self._result.update(area.get_rooms(thread_id))
+        remain_time = (time.time() - self._scan_start_time) / self._scanned_area * self._q.qsize()
+        print('线程 %d 获取完成，当前共 %d 个房源，队列剩余 %d 个区块，花费时间 %ds，预计剩余时间 %ds ' % (
+            thread_id, len(self._result), self._q.qsize(), time.time() - self._scan_start_time, remain_time))
+        # 告知结束一个任务
+        self._q.task_done()
 
 
 if __name__ == '__main__':
     grid_range = [115.7, 117.4, 39.4, 41.6]  # 北京市范围，扫描别的城市，只要修改经纬度范围即可 参数格式["lon_min,lon_max,lat_min,lat_max"]
 
     # 测试
-    # gm = GridManager(grid_range, min_area=1e10)
-    gm = GridManager(grid_range)
+    # gm = GridManager(grid_range, min_area=1e9)
+    gm = GridManager(grid_range, thread_num=16)
     all_rooms = gm.run()
     rooms = list(filter(lambda x: x["room_status"] != "ycz" and x["room_status"] != "yxd", all_rooms.values()))
     share_rooms = list(filter(lambda x: x["is_whole"] == 0, rooms))
